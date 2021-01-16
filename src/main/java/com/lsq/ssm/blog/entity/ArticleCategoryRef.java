489a9bb0c01570798a5fdbba5a9e0e8d498108aa
nono.liang <691454061@qq.com>
2021-01-16 23:08:50 +0800

    feat 【更新】 更新文件路径

---
+++ src/main/java/com/lsq/ssm/blog/entity/ArticleCategoryRef.java
@@ -0,0 +1,48 @@
+package com.lsq.ssm.blog.entity;
+
+import lombok.Data;
+
+import java.io.Serializable;
+
+/**
+ * 文章分类关联表
+ *
+ * @author liuyanzhao
+ */
+@Data
+public class ArticleCategoryRef implements Serializable{
+
+    private static final long serialVersionUID = -6809206515467725995L;
+
+    private Integer articleId;
+
+    private Integer categoryId;
+
+    public ArticleCategoryRef() {
+    }
+
+    public ArticleCategoryRef(Integer articleId, Integer categoryId) {
+        this.articleId = articleId;
+        this.categoryId = categoryId;
+    }
+
+    public static long getSerialVersionUID() {
+        return serialVersionUID;
+    }
+
+    public Integer getArticleId() {
+        return articleId;
+    }
+
+    public void setArticleId(Integer articleId) {
+        this.articleId = articleId;
+    }
+
+    public Integer getCategoryId() {
+        return categoryId;
+    }
+
+    public void setCategoryId(Integer categoryId) {
+        this.categoryId = categoryId;
+    }
+}
